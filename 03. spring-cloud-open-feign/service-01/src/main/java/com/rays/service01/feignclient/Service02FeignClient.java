9ed76093af9b80e50b147c38d724904b536b03b3
Raj kumar <rm70698@gmail.com>
2024-04-05 15:08:34 +0530

    done

---
+++ 03. spring-cloud-open-feign/service-01/src/main/java/com/rays/service01/feignclient/Service02FeignClient.java
@@ -0,0 +1,15 @@
+package com.rays.service01.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "service02", url = "http://localhost:8082")
+public interface Service02FeignClient {
+
+	@GetMapping("/service02/User/display")
+	public String display();
+
+	@PostMapping("/service02/User/submit")
+	public String submit();
+}
